b5ec8d3f596d2f09cf1ba975219f322a97805cd3
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2023-02-10 16:33:25 +0100

    pr changes

--- src/Exceptions/MissingTraitException.php
+++ src/Exceptions/MissingTraitException.php
@@ -4,6 +4,11 @@ namespace TeamNiftyGmbH\DataTable\Exceptions;
 
 final class MissingTraitException extends \Exception
 {
+    /**
+     * @param mixed $class
+     * @param string $trait
+     * @return static
+     */
     public static function create(mixed $class, string $trait): self
     {
         return new self($class . ' must use the ' . $trait . ' trait');
