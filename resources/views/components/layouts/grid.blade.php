764ce9d57f1d5aa74f179b895ba58bb841e1c1ae
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2023-11-05 13:34:08 +0100

    fix missing delay for spinner (has been fixed with livewire 3.1)

--- resources/views/components/layouts/grid.blade.php
+++ resources/views/components/layouts/grid.blade.php
@@ -44,7 +44,7 @@
         </div>
     </template>
     <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-        <div wire:loading class="absolute bottom-0 top-0 right-0 w-full">
+        <div wire:loading.delay.longer class="absolute bottom-0 top-0 right-0 w-full">
             <x-tall-datatables::spinner />
         </div>
         <template x-for="(record, index) in getData()">
@@ -103,7 +103,7 @@
             </template>
         @else
             <div x-intersect:enter="$wire.get('initialized') && $wire.loadMore()" class="w-full">
-                <x-button flat spinner wire:loading wire:target="loadMore" class="w-full">
+                <x-button flat spinner wire:loading.delay.longer wire:target="loadMore" class="w-full">
                     {{ __('Loading...') }}
                 </x-button>
             </div>
