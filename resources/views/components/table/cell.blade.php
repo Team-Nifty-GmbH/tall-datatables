e7ab50f5e1c7fac6adb40df098c4244f040defd5
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2023-12-18 17:41:48 +0100

    fix wire navigate

--- resources/views/components/table/cell.blade.php
+++ resources/views/components/table/cell.blade.php
@@ -1,6 +1,6 @@
 @props([
     'useWireNavigate' => false,
 ])
-<a {{ $useWireNavigate ? 'wire:navigate' : '' }} {{ $attributes->merge(['class' => 'align-top table-cell border-b border-slate-200 dark:border-slate-600 whitespace-normal px-3 py-4 text-sm']) }} >
+<a @if($useWireNavigate) x-on:click.prevent="$el.href && Livewire.navigate($el.href)" @endif {{ $attributes->merge(['class' => 'align-top table-cell border-b border-slate-200 dark:border-slate-600 whitespace-normal px-3 py-4 text-sm']) }} >
     {{ $slot ?? '' }}
 </a>
