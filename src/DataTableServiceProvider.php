ac10f2059d5785384fd5129913f32d509f969532
patrickweh <pw@team-nifty.com>
2025-04-03 08:41:00 +0200

    sort search result by scout weight

--- src/DataTableServiceProvider.php
+++ src/DataTableServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace TeamNiftyGmbH\DataTable;
 
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\DB;
