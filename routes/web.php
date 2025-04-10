fd235b24f0235a3c383d0e709d07b8afd8ab5d3e
patrickweh <pw@team-nifty.com>
2025-04-10 18:21:25 +0200

    add icon route to tall-datatables

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use TeamNiftyGmbH\DataTable\Controllers\AssetController;
+use TeamNiftyGmbH\DataTable\Controllers\IconController;
 
 Route::name('tall-datatables.')->prefix('/tall-datatables')->group(function (): void {
     Route::get('/assets/scripts', [AssetController::class, 'scripts'])->name('assets.scripts');
     Route::get('/assets/styles', [AssetController::class, 'styles'])->name('assets.styles');
+    Route::get('/icons/{name}/{variant?}', IconController::class)
+        ->where('variant', '(outline|solid)')
+        ->name('icons');
 });
