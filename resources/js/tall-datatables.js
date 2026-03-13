80ac842c9825bbaaae0cd4fdd9ab661fc544f6de
patrickweh <pw@team-nifty.com>
2026-03-13 17:18:35 +0100

    Reduce JS to minimal DOM-only Alpine component, delete formatters.js

--- resources/js/tall-datatables.js
+++ resources/js/tall-datatables.js
@@ -1,7 +1,4 @@
-import data_table from './components/data-table';
-import formatters from './components/formatters';
-
-window.formatters = formatters();
+import data_table from './components/data-table.js';
 
 document.addEventListener('alpine:init', () => {
     window.Alpine.data('data_table', data_table);
