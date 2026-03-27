2f3340c57ee4ed1b49fc4e71e98c6692f808d9c2
patrickweh <pw@team-nifty.com>
2026-03-27 16:43:24 +0100

    Use tallstackui:setup in head, reduce CI matrix to PHP 8.5 + LW 4

--- tests/resources/views/components/layouts/app.blade.php
+++ tests/resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>DataTable Browser Test</title>
-    {!! str_replace(' defer', '', app(\TallStackUi\Foundation\Support\Blade\Directives::class)->setup()) !!}
+    <tallstackui:setup />
     @dataTableStyles
     @dataTablesScripts
     @livewireStyles
