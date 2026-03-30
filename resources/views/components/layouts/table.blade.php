473548736f299b799a628b485d378ac287ae7c24
patrickweh <pw@team-nifty.com>
2026-03-30 11:12:04 +0200

    Only show actions chevron when rows are selected
    Track hasSelected via x-on:change on the table wrapper, checking for
    checked checkboxes in tbody. Chevron button and dropdown are hidden
    with x-show/x-cloak until at least one row is selected.

--- resources/views/components/layouts/table.blade.php
+++ resources/views/components/layouts/table.blade.php
@@ -17,7 +17,11 @@
     'allowSoftDeletes' => false,
     'showRestoreButton' => false,
 ])
-<div class="mt-3 flex flex-col" x-data="{ showSelectedActions: false }">
+<div
+    class="mt-3 flex flex-col"
+    x-data="{ showSelectedActions: false, hasSelected: false }"
+    x-on:change="hasSelected = $el.querySelectorAll('tbody input[type=checkbox]:checked, tbody input[type=hidden][value=true]').length > 0"
+>
     <div class="relative overflow-x-auto shadow ring-1 ring-black/5 sm:rounded-lg">
         <table class="dark:divide-secondary-700 dark:bg-secondary-800 min-w-full table-auto border-collapse divide-y divide-gray-300 rounded-md bg-white text-gray-500 dark:text-gray-50">
             <thead class="font-semibold" style="z-index: 9">
@@ -44,7 +48,7 @@
                                         :checked="in_array('*', $this->selected)"
                                         sm
                                     />
-                                    <div class="relative">
+                                    <div class="relative" x-show="hasSelected" x-cloak>
                                         <x-button
                                             color="secondary"
                                             light
